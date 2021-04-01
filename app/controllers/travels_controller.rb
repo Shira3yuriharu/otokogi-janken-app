90cdc2654e0648ff82a79df52dc906d3bb504a04
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-02 08:22:27 +0900

    ビューファイル修正②

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,5 +1,22 @@
 class TravelsController < ApplicationController
   def new
     @travel = Travel.new
+    @groups = Group.all
   end
+
+  def create
+    @travel = Travel.new(travel_params)
+    if @travel.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def travel_params
+    params.require(:travel).permit(:name, :name2, :start_day, :end_day, :groupname)
+  end
+
 end
