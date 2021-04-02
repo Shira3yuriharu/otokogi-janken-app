4e81148c48fa4e18e3c52114a080cc4e450ef0fe
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-03 08:25:08 +0900

    ①travel_selectsコントローラー実装②travel_selectモデルにバリデーション実装など

--- app/controllers/travel_selects_controller.rb
+++ app/controllers/travel_selects_controller.rb
@@ -1,5 +1,22 @@
 class TravelSelectsController < ApplicationController
+ 
   def new
     @travel_select = TravelSelect.new
   end
+
+  def create
+    @travel_select = TravelSelect.new(travel_select_params)
+    if @travel_select.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def travel_select_params
+    params.require(:travel_select).permit(:travel_id).merge(user_id: current_user.id)
+  end
+
 end
