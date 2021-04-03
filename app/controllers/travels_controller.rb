4c785f116825ebdf9009f49efe0994434640dc7b
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-03 20:48:07 +0900

    TopPageにCurrent_Userが参加している男気旅行一覧表示を実装

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,10 +1,22 @@
 class TravelsController < ApplicationController
   def new
     @travel = Travel.new
+    # @groups = current_user.Group.all
+    # current_user_id = current_user.id
+    # group_users = GroupUser.where(user_id: current_user_id)
+    # @group = Group.where(id: group_users.group_id)
+
+    # current_user_belong_to_group = GroupUser.where( user_id: current_user.id )
+    # @group = Group.where( id: "22")
+    # ↓groups→groupに変更
+    @group = current_user.groups.each do |group|
+    end
+
   end
 
   def create
     @travel = Travel.new(travel_params)
+    # binding.pry
     if @travel.save
       redirect_to root_path
     else
