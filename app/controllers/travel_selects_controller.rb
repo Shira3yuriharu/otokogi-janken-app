88d7318a9bbbb8823cd37d0c8bea084ea4d0939c
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-05 00:12:20 +0900

    TopPageのユーザー名と旅行名表示箇所修正など

--- app/controllers/travel_selects_controller.rb
+++ app/controllers/travel_selects_controller.rb
@@ -7,13 +7,7 @@ class TravelSelectsController < ApplicationController
     @groups = GroupUser.where(user_id: @user)
     @group_ids = @groups.select(:group_id)
     @travels = Travel.where(group_id: @group_ids)
-    # @travel2 = 0
-      # current_user.groups.each do |group|
-        # @travel = group.travels.each do |travel|
-          # @travel2 += @travel
-        # end
-      # end
-    # binding.pry
+
   end
 
   def create
