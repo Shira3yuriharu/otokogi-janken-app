c6cfc9b1377680b27fb830154dc2a73ea44410a3
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-06 21:59:35 +0900

    herokuエラー対応①ルーティングの記述変更②results_controllerにユーザーがジャンケンを記録する旅行を選択していない場合の分岐を設定

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,11 @@ class ResultsController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @travel_select_last = TravelSelect.where(user_id: @user).last
-    @travel_select_last_travel_id = @travel_select_last.travel_id
-    @travel_name = Travel.find(@travel_select_last_travel_id).name
+    if @travel_select_last.nil?
+      @travel_name = "男気グループ未登録の為、表示できません"
+    else
+      @travel_select_last_travel_id = @travel_select_last.travel_id
+      @travel_name = Travel.find(@travel_select_last_travel_id).name
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'results/index'
   root to:"results#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create]
