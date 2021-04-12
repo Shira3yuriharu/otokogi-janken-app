8d9330628c4a68493e921244b4eba51750d7940e
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-12 20:14:02 +0900

    ①checked.jsファイル作成②読み込み設定実施

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require("chartkick/chart.js")
+require("../checked")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
