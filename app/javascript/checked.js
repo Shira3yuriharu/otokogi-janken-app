a4aeea283e973d0ea47d12e6b7f703f29729a02d
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-17 20:49:51 +0900

    nickname毎に、金額と勝利回数の合計を非同期で表示

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -2,14 +2,34 @@ function check() {
   // 表示されているすべての旅行名称を取得している
   const travels = document.querySelectorAll(".travel");
 
-  const element1 = document.getElementsByClassName( "select-trip-chart-1" )[0] ;
-  const ele1 = element1.appendChild(document.createElement('p'))
-  const element2 = document.getElementsByClassName( "select-trip-chart-2" )[0] ;
-  const ele2 = element2.appendChild(document.createElement('p'))
-  const element3 = document.getElementsByClassName( "select-trip-chart-3" )[0] ;
-  const ele3 = element3.appendChild(document.createElement('p'))
-  const element4 = document.getElementsByClassName( "select-trip-chart-4" )[0] ;
-  const ele4 = element4.appendChild(document.createElement('p'))
+  // ユーザー１の定義
+  const element1_1 = document.getElementsByClassName( "nickname1" )[0] ;
+  const ele1_1 = element1_1.appendChild(document.createElement('p'))
+  const element1_2 = document.getElementsByClassName( "money1" )[0] ;
+  const ele1_2 = element1_2.appendChild(document.createElement('p'))
+  const element1_3 = document.getElementsByClassName( "times1" )[0] ;
+  const ele1_3 = element1_3.appendChild(document.createElement('p'))
+  // ユーザー2の定義
+  const element2_1 = document.getElementsByClassName( "nickname2" )[0] ;
+  const ele2_1 = element2_1.appendChild(document.createElement('p'))
+  const element2_2 = document.getElementsByClassName( "money2" )[0] ;
+  const ele2_2 = element2_2.appendChild(document.createElement('p'))
+  const element2_3 = document.getElementsByClassName( "times2" )[0] ;
+  const ele2_3 = element2_3.appendChild(document.createElement('p'))
+  // ユーザー3の定義
+  const element3_1 = document.getElementsByClassName( "nickname3" )[0] ;
+  const ele3_1 = element3_1.appendChild(document.createElement('p'))
+  const element3_2 = document.getElementsByClassName( "money3" )[0] ;
+  const ele3_2 = element3_2.appendChild(document.createElement('p'))
+  const element3_3 = document.getElementsByClassName( "times3" )[0] ;
+  const ele3_3 = element3_3.appendChild(document.createElement('p'))
+  // ユーザー4の定義
+  const element4_1 = document.getElementsByClassName( "nickname4" )[0] ;
+  const ele4_1 = element4_1.appendChild(document.createElement('p'))
+  const element4_2 = document.getElementsByClassName( "money4" )[0] ;
+  const ele4_2 = element4_2.appendChild(document.createElement('p'))
+  const element4_3 = document.getElementsByClassName( "times4" )[0] ;
+  const ele4_3 = element4_3.appendChild(document.createElement('p'))
 
   travels.forEach(function(travel) {
     if (travel.getAttribute("data-load") != null) {
@@ -60,35 +80,57 @@ function check() {
         }
 
         // 合計金額表示処理
-          ele1.innerHTML='';
-          ele2.innerHTML='';
-          ele3.innerHTML='';
-          ele4.innerHTML='';
+          // 前回の集計結果をリセット
+          ele1_1.innerHTML='';
+          ele1_2.innerHTML='';
+          ele1_3.innerHTML='';
+          ele2_1.innerHTML='';
+          ele2_2.innerHTML='';
+          ele2_3.innerHTML='';
+          ele3_1.innerHTML='';
+          ele3_2.innerHTML='';
+          ele3_3.innerHTML='';
+          ele4_1.innerHTML='';
+          ele4_2.innerHTML='';
+          ele4_3.innerHTML='';
 
-        let a = 0;
-        let flg = 0;
+        let name = null;
+        let money = 0;
+        let times = 0;
         const uniqueArray = [...new Set(nickname)];
         i2 = uniqueArray.length
         for (let step = 0; step < i2; step++) {
-          let a = 0;
+          let name = null;
+          let money = 0;
+          let times = 0;
           i3 = travel_select_pie.length
             for(let step2 = 0; step2<i3; step2++){
               if (uniqueArray[step] === travel_select_pie[step2]["nickname"]){
-                a = travel_select_pie[step2]["money"] + a
+                name = travel_select_pie[step2]["nickname"]
+                money = travel_select_pie[step2]["money"] + money
+                times = times + 1
               }
             }
-            console.log (a)
+            // console.log (a)
             if (step === 0){
-             ele1.appendChild(document.createTextNode((a)));
+             ele1_1.appendChild(document.createTextNode((name)));
+             ele1_2.appendChild(document.createTextNode((money)));
+             ele1_3.appendChild(document.createTextNode((times)));
             }
             if (step === 1){
-              ele2.appendChild(document.createTextNode((a)));
+              ele2_1.appendChild(document.createTextNode((name)));
+              ele2_2.appendChild(document.createTextNode((money)));
+              ele2_3.appendChild(document.createTextNode((times)));
              }
              if (step === 2){
-              ele3.appendChild(document.createTextNode((a)));
+              ele3_1.appendChild(document.createTextNode((name)));
+              ele3_2.appendChild(document.createTextNode((money)));
+              ele3_3.appendChild(document.createTextNode((times)));
              }
              if (step === 3){
-              ele4.appendChild(document.createTextNode((a)));
+              ele4_1.appendChild(document.createTextNode((name)));
+              ele4_2.appendChild(document.createTextNode((money)));
+              ele4_3.appendChild(document.createTextNode((times)));
              }
         }
 
