7bbccf21aabf255776d18ecf6af7611c2fd407e8
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-04-30 16:50:43 +0900

    旅行計画ページで終了日が開始日より早くならない様。バリデーション設定

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -7,4 +7,11 @@ class Travel < ApplicationRecord
   validates :start_day, presence: true
   validates :end_day, presence: true
   validates :group_id, presence: true
+
+  validate :end_day_check
+
+  def end_day_check
+    errors.add(:end_day, "は開始時刻より遅い時間を選択してください") if self.start_day > self.end_day
+  end
+
 end
