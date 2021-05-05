ec8f61e92e0531e8fc0192da20695dbdf7c3fa4e
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-05-05 12:33:18 +0900

    spotsコントローラー編集

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,7 +1,18 @@
 class SpotsController < ApplicationController
+
+  before_action :search_product, only: [:index, :search]
+
   def index
+    @spots = Spot.all
   end
 
   def search
+    @results = @p.result.includes(:category)
   end
+
+  private
+  def search_spot
+    @p = Spot.ransack(params[:q])
+  end
+
 end
