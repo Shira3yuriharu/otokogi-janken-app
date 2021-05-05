09ada926452082d265a2fcf5439d3a518ffce188
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-05-05 11:09:33 +0900

    マイグレーションファイル編集

--- db/migrate/20210505014207_create_spots.rb
+++ db/migrate/20210505014207_create_spots.rb
@@ -1,7 +1,10 @@
 class CreateSpots < ActiveRecord::Migration[6.0]
   def change
     create_table :spots do |t|
-
+      t.string :name, null: false
+      t.string :url, null: false
+      t.string :genre, null: false
+      t.references :category, foreign_key: true
       t.timestamps
     end
   end
